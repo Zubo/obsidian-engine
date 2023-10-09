5b374d3f813dd8afb7f08c81e961a65171908518
Ivo Zubovic <ivo.zubovic@gmail.com>
2023-10-09 17:07:51 +0200

    Scene asset info reading implemented

--- libs/asset/src/scene_asset_info.cpp
+++ libs/asset/src/scene_asset_info.cpp
@@ -1,18 +1,28 @@
-#include "obsidian/asset/asset_info.hpp"
-#include "obsidian/core/logging.hpp"
 #include <obsidian/asset/asset.hpp>
+#include <obsidian/asset/asset_info.hpp>
 #include <obsidian/asset/scene_asset_info.hpp>
 #include <obsidian/asset/utility.hpp>
+#include <obsidian/core/logging.hpp>
 
 #include <lz4.h>
 #include <nlohmann/json.hpp>
 
 #include <cassert>
+#include <exception>
 #include <utility>
 
 namespace obsidian::asset {
 
 bool readSceneAssetInfo(Asset const& asset, SceneAssetInfo& outSceneAssetInfo) {
+  try {
+    nlohmann::json json = nlohmann::json::parse(asset.json);
+    outSceneAssetInfo.unpackedSize = json[unpackedSizeJsonName];
+    outSceneAssetInfo.compressionMode = json[compressionModeJsonName];
+  } catch (std::exception const& e) {
+    OBS_LOG_ERR(e.what());
+    return false;
+  }
+
   return true;
 }
 
