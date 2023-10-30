1b45dcbed3ca924b5b6d4a77c4cab8b5122ca0a8
Ivo Zubovic <ivo.zubovic@gmail.com>
2023-10-30 21:47:33 +0100

    Added gtest into project. Task library skeleton

---
+++ libs/task/src/task_executor.cpp
@@ -0,0 +1,1 @@
+#include <obsidian/task/task_executor.hpp>
--- third_party/ThirdParty.cmake
+++ third_party/ThirdParty.cmake
@@ -144,3 +144,11 @@ target_include_directories(StbImage
 )
 
 FetchContent_MakeAvailable(fetch_json)
+
+FetchContent_Declare(fetch_gtest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.14.0
+    GIT_PROGRESS TRUE
+)
+
+FetchContent_MakeAvailable(fetch_gtest)
