e48cf90bf0784c30782c387bcc8676663fda10f9
Ivo Zubovic <ivo.zubovic@gmail.com>
2024-01-06 16:00:46 +0100

    Remove automatic launching of renderdoc UI every time the application is started

--- libs/renderdoc/src/renderdoc.cpp
+++ libs/renderdoc/src/renderdoc.cpp
@@ -11,19 +11,20 @@
 RENDERDOC_API_1_6_0* renderdocApi = nullptr;
 
 void loadRenderdocLibrary() {
-  if (void* mod = dlopen(RENDERDOC_PATH, RTLD_NOW | RTLD_LOCAL)) {
-    pRENDERDOC_GetAPI RENDERDOC_GetAPI =
-        (pRENDERDOC_GetAPI)dlsym(mod, "RENDERDOC_GetAPI");
-    int ret = RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_6_0,
-                               reinterpret_cast<void**>(&renderdocApi));
-    assert(ret == 1);
-  };
+  pRENDERDOC_GetAPI RENDERDOC_GetAPI = nullptr;
+
+  if (void* mod = dlopen("librenderdoc.so", RTLD_NOW | RTLD_NOLOAD)) {
+    RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)dlsym(mod, "RENDERDOC_GetAPI");
+  } else if (void* mod = dlopen("librenderdoc.so", RTLD_NOW)) {
+    RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)dlsym(mod, "RENDERDOC_GetAPI");
+  }
+
+  int ret = RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_6_0,
+                             reinterpret_cast<void**>(&renderdocApi));
+  assert(ret == 1);
 }
 
-void renderdoc::initRenderdoc() {
-  loadRenderdocLibrary();
-  renderdocApi->LaunchReplayUI(1, "");
-}
+void renderdoc::initRenderdoc() { loadRenderdocLibrary(); }
 
 void renderdoc::deinitRenderdoc() {
   // RENDERDOC_API_1_6_0::RemoveHooks() is not implemented on linux.
@@ -37,6 +38,12 @@ void renderdoc::beginCapture() {
 void renderdoc::endCapture() {
   assert(renderdocApi);
   renderdocApi->EndFrameCapture(NULL, NULL);
+
+  if (renderdocApi->IsTargetControlConnected()) {
+    renderdocApi->ShowReplayUI();
+  } else {
+    renderdocApi->LaunchReplayUI(1, "");
+  }
 }
 
 #else
