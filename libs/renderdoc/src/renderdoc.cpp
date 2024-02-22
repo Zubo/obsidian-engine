812896c888437cad9a43bc87efd86e0914fa7e10
Ivo Zubovic <ivo.zubovic@gmail.com>
2024-02-22 20:15:17 +0100

    Renderdoc module loading on windows

--- libs/renderdoc/src/renderdoc.cpp
+++ libs/renderdoc/src/renderdoc.cpp
@@ -4,7 +4,16 @@
 
 #include <cassert>
 #include <cstddef>
+
+#ifdef _WIN32
+#include <windows.h>
+
+#include <filesystem>
+#include <string>
+#else
 #include <dlfcn.h>
+#endif
+
 
 #include <renderdoc_app.h>
 
@@ -13,11 +22,21 @@ RENDERDOC_API_1_6_0* renderdocApi = nullptr;
 void loadRenderdocLibrary() {
   pRENDERDOC_GetAPI RENDERDOC_GetAPI = nullptr;
 
+#ifdef _WIN32
+  namespace fs = std::filesystem;
+  fs::path renderdocModulePath =
+      fs::path{RENDERDOC_PATH} / "renderdoc.dll";
+  std::string pathStr = renderdocModulePath.string();
+  HMODULE renderdocHModule = LoadLibraryA(pathStr.c_str());
+  RENDERDOC_GetAPI =
+      (pRENDERDOC_GetAPI)GetProcAddress(renderdocHModule, "RENDERDOC_GetAPI");
+#else
   if (void* mod = dlopen("librenderdoc.so", RTLD_NOW | RTLD_NOLOAD)) {
     RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)dlsym(mod, "RENDERDOC_GetAPI");
   } else if (void* mod = dlopen("librenderdoc.so", RTLD_NOW)) {
     RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)dlsym(mod, "RENDERDOC_GetAPI");
   }
+#endif
 
   int ret = RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_6_0,
                              reinterpret_cast<void**>(&renderdocApi));
@@ -27,6 +46,9 @@ void loadRenderdocLibrary() {
 void renderdoc::initRenderdoc() { loadRenderdocLibrary(); }
 
 void renderdoc::deinitRenderdoc() {
+#ifdef _WIN32
+  renderdocApi->RemoveHooks();
+#endif
   // RENDERDOC_API_1_6_0::RemoveHooks() is not implemented on linux.
 }
 
